3d2cdee90f9e64651f3a39d8d6d536b2d9d2a507
Davi Marques Gonçalves <davisigon@gmail.com>
2022-01-28 21:16:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ElCraco.App
---
+++ trampo.php
@@ -0,0 +1,24 @@
+<?php
+
+>
+<!DOCTYPE html>
+<html>
+<link href="index.css" rel="stylesheet">
+<title>El Craco 2.0</title>
+
+<body>
+
+    <h1>This is an El Craco O menage</h1>
+    <p>This is El Craco</p>
+    <img src="pp.jpeg" class="jo">
+    <img src="1.jpeg" class="ooo">
+    <img src="4.jpeg" class="j">
+
+</body>
+
+<footer>
+    <h2>El Craco is an famous artist in the mexico city</h2>
+    <h3>El Craco is so much friendly</h3>
+</footer>
+
+</html>
